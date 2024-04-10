3243849abb1c2ea2bc88be63da9601a3cf4b139f
Shubhangi Rastogi <70335325+shubhangi0301@users.noreply.github.com>
2024-04-10 22:11:21 +0530

    Update facedet.py

--- facedet.py
+++ facedet.py
@@ -1,6 +1,6 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier(r'C:\Users\Admin\OneDrive - vit.ac.in\Documents\VSCode\MISC\FaceDetection\index.xml')
+face_cascade = cv2.CascadeClassifier('index.xml')
 
 if face_cascade.empty():
     print("Error: Failed to load cascade classifier.")
@@ -16,7 +16,7 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (50, 50, 4), 10)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (50, 50, 4), 3)
 
     cv2.imshow('img', img)
     k = cv2.waitKey(30) & 0xff
